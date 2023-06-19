cbe521f1d3ab764ae15f44bb194d659cf67a2d13
UROOJ ISLAM <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2023-06-19 13:43:26 +0500

    Add files via upload

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+package JavaFullForPlacement;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        int bh;
+        System.out.println("Before swapping values of and b ");
+        System.out.println("Value of a:"+a+"\nValue of b:"+b);
+        bh=a;
+        a=b;
+        b=bh;
+        System.out.println("\nAfter Swapping values of a and b");
+        System.out.println("Value of a:"+a+"\nValue of b:"+b);
+    }
+}
